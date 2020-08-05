38ec276e28de5944b584289b610c67002f0b0345
Bennett1234 <43047495+Bennett1234@users.noreply.github.com>
2020-08-05 17:11:05 -0400

    Create 176&177 Highes Salary.sql

---
+++ 176&177 Highes Salary.sql
@@ -0,0 +1,25 @@
+176.Write a SQL query to get the second highest salary from the Employee table.
+
+SELECT (ISNULL((SELECT DISTINCT Salary
+from(
+SELECT Salary,
+    RANK() OVER(ORDER BY Salary DESC) AS ranking
+FROM Employee ) as EM
+WHERE EM.ranking = 2),NULL)) as SecondHighestSalary
+
+
+
+
+177. Write a SQL query to get the nth highest salary from the Employee table.
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT max(Salary)
+      FROM Employee x
+      WHERE (N-1) = (SELECT COUNT(Salary)
+          FROM (SELECT DISTINCT Salary 
+                     FROM Employee) y
+          WHERE y.Salary > x.Salary)
+  );
+END
