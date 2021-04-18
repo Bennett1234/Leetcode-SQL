eedda2a2ba3707cd07b59fe538fc1c856809d9cd
Bennett1234 <43047495+Bennett1234@users.noreply.github.com>
2021-04-17 23:37:01 -0400

    Update 180 Consecutive number.sql

--- 180 Consecutive number.sql
+++ 180 Consecutive number.sql
@@ -5,3 +5,10 @@ WHERE a.Num-b.Num=0
 AND b.Num-c.Num = 0
 AND c.Id-b.Id = 1
 AND b.Id-a.Id = 1
+
+SELECT DISTINCT a.Num AS ConsecutiveNums FROM Logs AS a
+INNER JOIN Logs AS b ON  a.Id + 1 = b.Id
+INNER JOIN Logs AS c ON a.Id + 2 = c.Id
+WHERE a.Num = b.Num
+AND b.Num = c.Num
+-- must include distinct other wise will return multiple number when more than 3 times consecutively showed up
