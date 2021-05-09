44217699a1fa7a02136e9f15df203190245a066e
Bennett1234 <43047495+Bennett1234@users.noreply.github.com>
2021-05-09 18:40:25 -0400

    Update 569. Median Employee Salary.sql

--- 569. Median Employee Salary.sql
+++ 569. Median Employee Salary.sql
@@ -1,4 +1,9 @@
 SELECT Id, Company, Salary
 FROM(SELECT Id, Company, Salary,
 ROW_NUMBER () OVER (PARTITIONBY Company ORDERBY Salary) AS RN ,
-COUNT(Id) OVER (PARTITIONBY Company)ASCOUNTSFROMEmployee)TWHERERNBETWEENCOUNTS/20ANDCOUNTS/20+1ORDERBY23ASC
+COUNT(Id) OVER (PARTITION BY Company) AS COUNTS FROM Employee)T WHERE RN BETWEEN COUNTS/2 AND COUNTS/2+1 ORDER BY 23 ASC
+
+/***
+COUNT(*) OVER (ORDER BY U.userid) AS CNT calulates a "running count" - the count until "that" row. 
+If you want to count all rows in the complete result, use the window function without the order by
+***/
