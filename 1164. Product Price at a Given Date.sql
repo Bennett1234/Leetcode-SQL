6dc0cbfd9df347670d132de18712a56ce4efd37b
Bennett1234 <43047495+Bennett1234@users.noreply.github.com>
2025-01-16 22:48:57 -0500

    Update 1164. Product Price at a Given Date.sql

--- 1164. Product Price at a Given Date.sql
+++ 1164. Product Price at a Given Date.sql
@@ -1,15 +1,9 @@
-select p1.product_id, p1.new_price as price
-from Products p1
-join (select product_id, max(change_date) as change_date
-     from Products as p2
-     where changge_date <= '2019-08-06'
-     group by product_id) as p2
-    on p2.product_id = p1.product_id and p1.changge_date = p2.change_date
+with base_table as (
+    select *, rank() over(partition by product_id order by change_date desc) as ranking
+    from products
+    where change_date <= '2019-08-16'
+    )
 
-union
-
-select product_id, 10 as price
-from Products
-group by product_id
-having min(change_date) > '2019-08-16'
-order by price desc
+select A.product_id, isnull(new_price, 10) as price
+from (select distinct product_id from products) A 
+left join (select * from base_table where ranking = 1) B on A.product_id = B.product_id
