05b8fc54307e529edb241ef541db7d9e18e41f74
Bennett1234 <43047495+Bennett1234@users.noreply.github.com>
2025-03-08 16:30:46 -0500

    Update 1193. Monthly Transactions I.sql

--- 1193. Monthly Transactions I.sql
+++ 1193. Monthly Transactions I.sql
@@ -1,12 +1,12 @@
-SELECT DATE_FORMAT(a.trans_date, "%Y-%m") AS month, a.country,
-COUNT(a.id) AS trans_count,
-(select count(*) from Transactions as b where b.state = 'approved' and DATE_FORMAT(b.trans_date, "%Y-%m") = DATE_FORMAT(a.trans_date, "%Y-%m") and a.country = b.country) as 
-approved_count
-SUM(a.amount) AS trans_total_amount,
-(select SUM(COALESCE(c.amount,0)) from Transactions as c where c.state = 'approved' and DATE_FORMAT(c.trans_date, "%Y-%m") = DATE_FORMAT(a.trans_date, "%Y-%m") and a.country = c.country) as 
-AS approved_total_amount 
-FROM Transactions AS a
-GROUP BY DATE_FORMAT(a.trans_date, "%Y-%m"), a.country;
+select
+left(trans_date,7) as month
+,country
+,count(*) as trans_count
+,sum(case when state = 'approved' then 1 else 0 end) as approved_count
+,sum(amount) as trans_total_amount
+,sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by country, left(trans_date,7)
 
 
 
