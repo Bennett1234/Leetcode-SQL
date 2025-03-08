44298ed307c87432ebee358ae590406039b0d18f
Bennett1234 <43047495+Bennett1234@users.noreply.github.com>
2025-03-08 11:25:28 -0500

    Update 1045. Customers Who Bought All Products.sql

--- 1045. Customers Who Bought All Products.sql
+++ 1045. Customers Who Bought All Products.sql
@@ -3,3 +3,10 @@ LEFT JOIN Customer AS b
 ON a.product_key = b.product_key
 GROUP BY b.customer_id
 HAVING COUNT(DISTINCT b.product_key) = (SELECT COUNT(*) FROM Product)
+
+
+
+SELECT customer_id
+FROM Customer
+group by customer_id
+having count(distinct product_key) = (select count(*) from Product)
