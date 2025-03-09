76793de4c1a93f86415f7f3a42b05b38baa27225
Bennett1234 <43047495+Bennett1234@users.noreply.github.com>
2025-03-09 16:04:16 -0400

    Update 1321. Restaurant Growth.sql

--- 1321. Restaurant Growth.sql
+++ 1321. Restaurant Growth.sql
@@ -7,3 +7,29 @@ from
 	from (select visited_on, sum(amount) amount from Customer GROUP BY visited_on) a
 	) b
 where b.rnk >= 6;
+
+
+
+
+with temp as (
+select
+visited_on
+,sum(amount) as amount
+from Customer
+group by visited_on
+
+
+)
+,
+ temp2 as(
+select
+visited_on,
+sum(amount) over(order by visited_on rows between 6 preceding and current row) as amount,
+round(avg(amount) over(order by visited_on rows between 6 preceding and current row),2) as average_amount
+,row_number() over(order by visited_on) as rk
+from temp
+order by visited_on)
+
+select visited_on, amount, average_amount
+from temp2
+where rk > 6
